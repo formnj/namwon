749438c5b38b6ea90dae2147d2642a6ebefe3099
jonghwan <subdiary@daum.net>
2023-11-16 17:03:16 +0900

    test

--- js/swiper_appy.js
+++ js/swiper_appy.js
@@ -1,13 +1,16 @@
 var Swipers = [];
+var interleaveOffset = 0.5;
+var parallaxOptions;
 $('.swiper-container').each(function(i) {
+    $(this).attr('swiper-index',i); //Attribute for swiper control
 
     if($(this).find('[class*="swiper-button-"]').length > 0){
         nextEl = '.swiper-button-next-'+(i);
         prevEl = '.swiper-button-prev-'+(i);
 
         navigation = {
-            nextEl: nextEl,   //⭐버튼 설정
-            prevEl: prevEl,   //⭐버튼 설정
+            nextEl: nextEl,   //버튼 설정
+            prevEl: prevEl,   //버튼 설정
         };
 
         for(var j=0; j<$(this).find('[class*="swiper-button-"]').length; j++){
@@ -27,66 +30,67 @@ $('.swiper-container').each(function(i) {
         pagination = {}
     }
 
-    if($(this).hasClass('parallax')){
-        var interleaveOffset = 0.5;
-        var parallaxOptions = {
-            loop: true,
-            speed: 1000,
-            parallax: true,
-            autoplay: {
-                delay: 500,
-                disableOnInteraction: false,
-            },
-            watchSlidesProgress: true,
-            navigation: {
-                nextEl: nextEl,   //⭐버튼 설정
-                prevEl: prevEl,   //⭐버튼 설정
-            },
-            pagination,
-            on: {
-                progress: function() {
-                    var swiper = this;
-                    for (var i = 0; i < swiper.slides.length; i++) {
-                        var slideProgress = swiper.slides[i].progress;
-                        var innerOffset = swiper.width * interleaveOffset;
-                        var innerTranslate = slideProgress * innerOffset;
-                        swiper.slides[i].querySelector(".item").style.transform =
-                        "translate3d(" + innerTranslate + "px, 0, 0)";
-                    }      
-                },
-
-                touchStart: function() {
+    parallaxOptions = {
+        loop: true,
+        speed: 1000,
+        parallax: true,
+        autoplay: {
+            delay: 500,
+            disableOnInteraction: false,
+        },
+        watchSlidesProgress: true,
+        navigation: {
+            nextEl: nextEl,
+            prevEl: prevEl,
+        },
+        pagination,
+        on: {
+            progress: function() {
                 var swiper = this;
                 for (var i = 0; i < swiper.slides.length; i++) {
-                    swiper.slides[i].style.transition = "";
-                }
-                },
+                    var slideProgress = swiper.slides[i].progress;
+                    var innerOffset = swiper.width * interleaveOffset;
+                    var innerTranslate = slideProgress * innerOffset;
+                    swiper.slides[i].querySelector(".item").style.transform =
+                    "translate3d(" + innerTranslate + "px, 0, 0)";
+                }      
+            },
 
-                setTransition: function(speed) {
-                    var swiper = this;
-                    for (var i = 0; i < swiper.slides.length; i++) {
-                        swiper.slides[i].style.transition = speed + "ms";
-                        swiper.slides[i].querySelector(".item").style.transition =
-                        speed + "ms";
-                    }
+            touchStart: function() {
+            var swiper = this;
+            for (var i = 0; i < swiper.slides.length; i++) {
+                swiper.slides[i].style.transition = "";
+            }
+            },
+
+            setTransition: function(speed) {
+                var swiper = this;
+                for (var i = 0; i < swiper.slides.length; i++) {
+                    swiper.slides[i].style.transition = speed + "ms";
+                    swiper.slides[i].querySelector(".item").style.transition =
+                    speed + "ms";
                 }
             }
-        };
+        }
+    }
+    defaultOptions = {
+        loop: true,
+        speed: 1000,
+        navigation,
+        pagination,
+        autoplay: {
+            delay: 500,
+            disableOnInteraction: false,
+        },
+        lazy : {
+            loadPrevNext : true // 이전, 다음 이미지는 미리 로딩
+        },
+    }
 
+    if($(this).hasClass('parallax')){
         Swipers[i] = new Swiper($(this),parallaxOptions);
     } else {
-        Swipers[i] = new Swiper($(this),{	//⭐아이디 설정
-            loop: true,
-            navigation,
-            pagination,
-            autoplay: {
-                delay: 500,
-                disableOnInteraction: false,
-            },
-            lazy : {
-                loadPrevNext : true // 이전, 다음 이미지는 미리 로딩
-            },
-        });
+        Swipers[i] = new Swiper($(this),defaultOptions);
     }
 
     $(this).find('.autoplay').click(function(){
@@ -99,5 +103,4 @@ $('.swiper-container').each(function(i) {
         }
     });
 });
-
 // var swiper = new Swiper(".swiper-container", parallaxOptions);
